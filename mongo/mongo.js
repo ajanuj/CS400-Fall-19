d895e9ceb6d4fef18f7ebf80448d84e032b3a579
Anuj Jain <43014915+ajanuj@users.noreply.github.com>
2019-11-30 16:47:37 -0500

    PS6 done
    I just created a separate mongo file and used code from stackoverflow

---
+++ mongo/mongo.js
@@ -0,0 +1,20 @@
+//This code is from https://stackoverflow.com/questions/24621940/how-to-properly-reuse-connection-to-mongodb-across-nodejs-application-and-module
+const mongoClient = require('mongodb').MongoClient;
+const config = require("../config.json");
+const mongoURL = config.db.host;
+
+let _db;
+
+module.exports = {
+
+    connect: function( callback ) {
+        mongoClient.connect( mongoURL,  { useNewUrlParser: true }, function( err, client ) {
+            _db  = client.db('Weather');
+            return callback( err );
+        } );
+    },
+    getDB: () => {
+        return _db; }
+
+
+}
